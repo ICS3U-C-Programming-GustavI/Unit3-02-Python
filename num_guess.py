24de6f898f0af0052b85b9d7010f035ffe90ae05
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-03-18 16:13:47 +0000

    Style fixes

--- num_guess.py
+++ num_guess.py
@@ -3,7 +3,8 @@
 # Created on: March 18, 2025
 # This program executes a number guessing game.
 
-import random  # Import the random module 
+import random  # Import the random module
+
 
 def main():
     # Get user's guess
@@ -16,8 +17,11 @@ def main():
     # Check if the number is right or wrong
     if number_guess == chosen_number:
         print("You got the right number!")
-    if number_guess != chosen_number: 
-        print("You did not get the right number. The correct number was:", chosen_number)  
+    if number_guess != chosen_number:
+        print(
+            "You did not get the right number. The correct number was:", chosen_number
+        )
+
 
 if __name__ == "__main__":
     main()
